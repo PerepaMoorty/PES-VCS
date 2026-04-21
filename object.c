b5dc768ab01e276a211f7d8258ccedf995c8a239
Moorty Perep <moortyperepa06@gmail.com>
2026-04-21 10:40:13 +0530

    Phase 1: Implement header construction and SHA-256 hashing in object_write

--- object.c
+++ object.c
@@ -107,4 +107,4 @@ int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out
 int object_read(const ObjectID *id, ObjectType *type_out, void **data_out, size_t *len_out) {
     (void)id; (void)type_out; (void)data_out; (void)len_out;
     return -1;
-}
+}   
